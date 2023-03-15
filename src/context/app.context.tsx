bc5053d79d38794d35e6351c1a0fac5e6697abf1
owlsua <allatsnow@gmail.com>
2023-03-15 20:39:11 +0100

    chore: updates app context with new values

--- src/context/app.context.tsx
+++ src/context/app.context.tsx
@@ -1,14 +1,25 @@
 import { createContext, PropsWithChildren, useState } from 'react';
 import { useLocalObservable } from 'mobx-react-lite';
 import CommandsStore from '@/stores/commandsStore';
+import ExternalLink from '@/components/ExternalLink/ExternalLink';
+import Welcome from '@/components/Welcome/Welcome';
+import Help from '@/components/Help/Help';
+
+interface IComponents {
+  [key: string]: JSX.Element | string;
+}
 export interface IAppContext {
+  components: IComponents;
   commands: any;
   inputValue: string;
+  availableCommands: string[];
   setInputValue: (inputValue: string) => void;
 }
 
 const defaultValues: IAppContext = {
+  components: {},
   commands: {},
+  availableCommands: [],
   inputValue: '',
   setInputValue: () => {},
 };
@@ -21,12 +32,25 @@ export const AppContextProvider = ({ children }: PropsWithChildren<any>) => {
     CommandsStore.create({ commands: [{ value: 'about' }] }),
   );
 
+  const components: IComponents = {
+    help: '',
+    about: '',
+    github: '',
+    linkedin: '',
+    email: '',
+    // add more commands here
+  };
+
+  const availableCommands = Object.keys(components);
+
   return (
     <AppContext.Provider
       value={{
         commands,
         inputValue,
         setInputValue,
+        components,
+        availableCommands,
       }}
     >
       {children}
