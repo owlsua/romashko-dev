d724df278e074b2d3c5c92554a1ac9efe14ef17c
owlsua <allatsnow@gmail.com>
2023-03-15 17:07:07 +0100

    feat: add app context

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,10 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <meta name="Romashko" content="Romashko - web developer" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1"
+        ></meta>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
@@ -12,7 +16,7 @@ export default function Document() {
           crossOrigin={'crossorigin'}
         />
         <link
-          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=VT323&display=swap"
           rel="stylesheet"
         />
       </Head>
