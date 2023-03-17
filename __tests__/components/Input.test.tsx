49976994d83ea594ec0cffdf5d4eb75d6ad9a769
Alexander <69318224+owlsua@users.noreply.github.com>
2023-03-17 22:08:24 +0100

    Prompt (#3)
    * feat: add prompt
    
    * test: add unit tests for prompt

--- __tests__/components/Input.test.tsx
+++ __tests__/components/Input.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import Input from '@/components/Input/Input';
 import { AppContextProvider } from '@/context/app.context';
 
