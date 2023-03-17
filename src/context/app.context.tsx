2ea4a1f0227025e2cd14b0562415c0af53f11311
owlsua <allatsnow@gmail.com>
2023-03-17 19:44:38 +0100

    chore: update app context

--- src/context/app.context.tsx
+++ src/context/app.context.tsx
@@ -23,33 +23,36 @@ const defaultValues: IAppContext = {
 
 export const AppContext = createContext(defaultValues);
 
-export const AppContextProvider = ({ children }: PropsWithChildren<any>) => {
+export const AppContextProvider = ({
+  value: propValue,
+  children,
+}: PropsWithChildren<any>) => {
   const [inputValue, setInputValue] = useState<string>('');
   const commands = useLocalObservable(() =>
     CommandsStore.create({ commands: [{ value: 'about' }] }),
   );
 
   const components: IComponents = {
-    help: '',
-    about: '',
-    github: '',
-    linkedin: '',
-    email: '',
+    help: 'help',
+    about: 'about',
+    github: 'github',
+    linkedin: 'linkedin',
+    email: 'email',
     // add more commands here
   };
 
   const availableCommands = Object.keys(components);
 
+  const value = {
+    commands,
+    inputValue,
+    setInputValue,
+    components,
+    availableCommands,
+  };
+
   return (
-    <AppContext.Provider
-      value={{
-        commands,
-        inputValue,
-        setInputValue,
-        components,
-        availableCommands,
-      }}
-    >
+    <AppContext.Provider value={propValue || value}>
       {children}
     </AppContext.Provider>
   );
