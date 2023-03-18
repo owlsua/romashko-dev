8e6378a8947e0167c1171f5b1da467929acfbca5
owlsua <allatsnow@gmail.com>
2023-03-18 21:06:08 +0100

    test: add unit tests for commandOutcome

---
+++ __tests__/components/CommandOutcome.test.tsx
@@ -0,0 +1,38 @@
+import { render, screen } from '@testing-library/react';
+import CommandOutcome from '@/components/CommandOutcome/CommandOutcome';
+import { AppContextProvider } from '@/context/app.context';
+
+// const existCommand = { value: 'welcome' };
+const existCommand = 'welcome';
+const notExistCommand = 'notExist';
+const components = {
+  welcome: 'welcome',
+};
+
+test('renders CommandOutcome', () => {
+  render(
+    <AppContextProvider value={{ components: components }}>
+      <CommandOutcome command={existCommand} />
+    </AppContextProvider>,
+  );
+
+  const outcome = screen.getByTestId('commandOutcome');
+  expect(outcome).toBeInTheDocument();
+
+  expect(outcome).toHaveTextContent(existCommand);
+});
+
+test('renders CommandOutcome with not exist command', () => {
+  render(
+    <AppContextProvider value={{ components: components }}>
+      <CommandOutcome command={notExistCommand} />
+    </AppContextProvider>,
+  );
+
+  const notFound = `Command not found: ${notExistCommand}. Try help to get started.`;
+
+  const outcome = screen.getByTestId('commandOutcome');
+  expect(outcome).toBeInTheDocument();
+
+  expect(outcome).toHaveTextContent(notFound);
+});
--- src/components/CommandOutcome/CommandOutcome.tsx
+++ src/components/CommandOutcome/CommandOutcome.tsx
@@ -4,17 +4,21 @@ import { AppContext } from '@/context/app.context';
 import styles from './styles.module.css';
 
 interface CommandOutcomeProps {
-  command: { value: string };
+  command: string;
 }
 
 const CommandOutcome = ({ command }: CommandOutcomeProps) => {
   const { components } = useContext(AppContext);
-  if (command.value === '') return null;
+  if (command === '') return null;
 
-  const component = components[command.value];
-  const notFound = `Command not found: ${command.value}. Try help to get started.`;
+  const component = components[command];
+  const notFound = `Command not found: ${command}. Try help to get started.`;
 
-  return <div className={styles.commandOutcome}>{component || notFound}</div>;
+  return (
+    <div className={styles.commandOutcome} data-testid="commandOutcome">
+      {component || notFound}
+    </div>
+  );
 };
 
 export default CommandOutcome;
