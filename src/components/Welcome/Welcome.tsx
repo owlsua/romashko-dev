3b003d45eb2726e0fecb7d48ae42e78e4ddd240f
owlsua <allatsnow@gmail.com>
2023-03-18 17:30:24 +0100

    feat: add welcome

---
+++ src/components/Welcome/Welcome.tsx
@@ -0,0 +1,19 @@
+import Title from '@/components/Title/Title';
+import About from '@/components/About/About';
+
+import styles from './styles.module.css';
+
+const aboutContent =
+  "I'm Alexander Romashko and this is my personal website where you can explore my projects and learn more about me by interacting with the app using a command-line interface.";
+const title = ['Romashko', '.dev'];
+
+const Welcome = () => {
+  return (
+    <div className={styles.welcome}>
+      <Title title={title} />
+      <About content={aboutContent} />
+    </div>
+  );
+};
+
+export default Welcome;
