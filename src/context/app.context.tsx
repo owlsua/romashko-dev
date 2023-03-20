911bf2a600be941f00fd22d475ea4063d9e3da4d
owlsua <allatsnow@gmail.com>
2023-03-20 15:52:18 +0100

    chore: update app context

--- src/context/app.context.tsx
+++ src/context/app.context.tsx
@@ -2,6 +2,11 @@ import { createContext, PropsWithChildren, useState } from 'react';
 import { useLocalObservable } from 'mobx-react-lite';
 import CommandsStore from '@/stores/commandsStore';
 
+import Welcome from '@/components/Welcome/Welcome';
+import Help from '@/components/Help/Help';
+import About from '@/components/About/About';
+import ExternalLink from '@/components/ExternalLink/ExternalLink';
+
 interface IComponents {
   [key: string]: JSX.Element | string;
 }
@@ -11,6 +16,7 @@ interface IAppContext {
   inputValue: string;
   availableCommands: string[];
   setInputValue: (inputValue: string) => void;
+  aboutContent: string;
 }
 
 const defaultValues: IAppContext = {
@@ -19,6 +25,7 @@ const defaultValues: IAppContext = {
   availableCommands: [],
   inputValue: '',
   setInputValue: () => {},
+  aboutContent: '',
 };
 
 export const AppContext = createContext(defaultValues);
@@ -32,13 +39,22 @@ export const AppContextProvider = ({
     CommandsStore.create({ commands: [{ value: 'welcome' }] }),
   );
 
+  const aboutContent =
+    "I'm Alexander Romashko and this is my personal website where you can explore my projects and learn more about me by interacting with the app using a command-line interface.";
+  const githubLink = 'https://github.com/owlsua';
+  const githubMessage = 'opening Github...';
+  const linkedinLink = 'https://www.linkedin.com/in/owlsua/';
+  const linkedinMessage = 'opening LinkedIn...';
+  const emailLink = 'mailto:allatsnow@gmail.com';
+  const emailMessage = 'opening email...';
+
   const components: IComponents = {
-    welcome: 'welcome',
-    help: 'help',
-    about: 'about',
-    github: 'github',
-    linkedin: 'linkedin',
-    email: 'email',
+    welcome: <Welcome />,
+    help: <Help />,
+    about: <About content={aboutContent} />,
+    github: <ExternalLink link={githubLink} message={githubMessage} />,
+    linkedin: <ExternalLink link={linkedinLink} message={linkedinMessage} />,
+    email: <ExternalLink link={emailLink} message={emailMessage} />,
     // add more commands here
   };
 
@@ -50,6 +66,7 @@ export const AppContextProvider = ({
     setInputValue,
     components,
     availableCommands,
+    aboutContent,
   };
 
   return (
