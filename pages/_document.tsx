359da7bdbc988c2db2d49ba281fab3f592dcc767
Alexander <69318224+owlsua@users.noreply.github.com>
2026-01-08 22:13:56 +0100

    CI/CD (#30)
    * chore: cicd fix
    
    * deps: update dependencies
    
    * config: udpate configs
    
    * test: udpate unit tests

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,10 +5,6 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <meta name="Romashko" content="Romashko - web developer" />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, maximum-scale=1"
-        ></meta>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           href="https://fonts.cdnfonts.com/css/cascadia-code"
