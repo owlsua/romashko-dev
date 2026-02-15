0bbb7b29cab830b2e4f5892bc4e9a647df3993ad
owlsua <allatsnow@gmail.com>
2026-02-15 20:54:42 +0100

    refactoring

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
 import '@/styles/global.css';
 import type { AppProps } from 'next/app';
 import { AppContextProvider } from '@/context/app.context';
+import { WeatherContextProvider } from '@/context/weather.context';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <AppContextProvider>
-      <Component {...pageProps} />
-    </AppContextProvider>
+    <WeatherContextProvider>
+      <AppContextProvider>
+        <Component {...pageProps} />
+      </AppContextProvider>
+    </WeatherContextProvider>
   );
 }
